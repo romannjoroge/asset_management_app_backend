33ae4f36d177297a09be8b945ab1841c7f8c0981
Roman Njoroge <roman.njoroge@njuguna.com>
2022-12-30 18:55:03 +0300

    refactor: change test folder name to Roman Test because it does not have unit tests

--- test/async_test/app.js
+++
@@ -1,8 +0,0 @@
-// To start an app listening on port 5000 and have a /test route
-const express = require('express')
-app = express()
-const testRoute = require('./routes/test')
-
-app.use('/test', testRoute)
-
-app.listen(4000, () => console.log('Server is listening on port 4000.....'))
--- test/export_test.js
+++
@@ -1,8 +0,0 @@
-const pool = require('./connection_test')
-
-const query1 = 'SELECT * FROM assets.item'
-
-pool.query(query1, (error, result)=>{
-    if (error) throw error
-    console.log(result.rows)
-})
