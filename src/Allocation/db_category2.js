065c51040dc959636a10ef94df680dbefdaaf43b
Roman Njoroge <roman.njoroge@njuguna.com>
2023-01-03 13:32:49 +0300

    feat: add SQL command to update category's parent folder ID

--- src/Allocation/db_category2.js
+++ src/Allocation/db_category2.js
@@ -3,13 +3,15 @@ const getCategoryID = 'SELECT ID FROM Category WHERE name = $1'
 const addStraightLineDepreciationEntry = 'INSERT INTO DepreciationPerYear (categoryID, value) VALUES ($1, $2)'
 const addWrittenValueDepreciationEntry = 'INSERT INTO DepreciationPercent (categoryID, percentage) VALUES ($1, $2)'
 const updateCategoryName = "UPDATE Category SET name = $1 WHERE id = $2";
+const updateFolderID = "UPDATE Category SET parentFolderID = $1 WHERE id = $2";
 
 let categoryTable = {
     add: addCategory,
     getID: getCategoryID,
     addStraight: addStraightLineDepreciationEntry,
     addWritten: addWrittenValueDepreciationEntry,
-    updateCategoryName: updateCategoryName
+    updateCategoryName: updateCategoryName,
+    updateFolderID: updateFolderID,
 }
 
 module.exports = categoryTable;
