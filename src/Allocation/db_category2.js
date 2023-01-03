58b3842782791123dcc0f5c130be2152c6fc1abf
Roman Njoroge <roman.njoroge@njuguna.com>
2023-01-03 16:40:56 +0300

    feat: add command to delete entry from depreciationPerYear Table

--- src/Allocation/db_category2.js
+++ src/Allocation/db_category2.js
@@ -5,8 +5,9 @@ const addWrittenValueDepreciationEntry = 'INSERT INTO DepreciationPercent (categ
 const updateCategoryName = "UPDATE Category SET name = $1 WHERE id = $2";
 const updateFolderID = "UPDATE Category SET parentFolderID = $1 WHERE id = $2";
 const updateDepreciationType = "UPDATE Category SET depreciationType = $1 WHERE id = $2";
-const insertDepreciationPerYear = "INSERT INTO DepreciationPerYear(categoryID, value) VALUES ($1, $2)";
-const insertDepreciationPercent = "INSERT INTO DepreciationPercent(categoryID, percentage) VALUES ($1, $2)";
+const insertDepreciationPerYear = "INSERT INTO DepreciationPerYear (categoryID, value) VALUES ($1, $2)";
+const insertDepreciationPercent = "INSERT INTO DepreciationPercent (categoryID, percentage) VALUES ($1, $2)";
+const deleteDepreciationPerYear = "DELETE FROM DepreciationPerYear WHERE categoryID = $1";
 
 let categoryTable = {
     add: addCategory,
@@ -18,6 +19,7 @@ let categoryTable = {
     updateDepreciationType: updateDepreciationType,
     insertDepreciationPerYear: insertDepreciationPerYear,
     insertDepreciationPercent: insertDepreciationPercent,
+    deleteDepreciationPerYear: deleteDepreciationPerYear
 }
 
 module.exports = categoryTable;
