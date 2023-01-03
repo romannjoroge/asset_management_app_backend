f5c9c49d28bec21134e5953e64e1a7b95c8aac31
Roman Njoroge <roman.njoroge@njuguna.com>
2023-01-03 16:16:22 +0300

    feat: add command to update depreciationType of a category

--- src/Allocation/db_category2.js
+++ src/Allocation/db_category2.js
@@ -4,6 +4,7 @@ const addStraightLineDepreciationEntry = 'INSERT INTO DepreciationPerYear (categ
 const addWrittenValueDepreciationEntry = 'INSERT INTO DepreciationPercent (categoryID, percentage) VALUES ($1, $2)'
 const updateCategoryName = "UPDATE Category SET name = $1 WHERE id = $2";
 const updateFolderID = "UPDATE Category SET parentFolderID = $1 WHERE id = $2";
+const updateDepreciationType = "UPDATE Category SET depreciationType = $1 WHERE id = $2";
 
 let categoryTable = {
     add: addCategory,
@@ -12,6 +13,7 @@ let categoryTable = {
     addWritten: addWrittenValueDepreciationEntry,
     updateCategoryName: updateCategoryName,
     updateFolderID: updateFolderID,
+    updateDepreciationType: updateDepreciationType,
 }
 
 module.exports = categoryTable;
