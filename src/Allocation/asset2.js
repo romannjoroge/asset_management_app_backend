826e1d8084cc1e4b775a3fd839553056ea5087e4
Roman Njoroge <roman.njoroge@njuguna.com>
2023-02-06 17:40:28 +0300

    feat: creating asset module

---
+++ src/Allocation/asset2.js
@@ -0,0 +1,6 @@
+// Importing the database bool from db2.js. This will allow me to connect to the database
+const pool = require('../../db2');
+
+// Importing custom classes
+const MyError = require('../../utility/myError');
+const Folder = require('./folder');
