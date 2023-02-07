4a2a7f2f1f55c9e66d5f587339c5b0c833262d7c
Roman Njoroge <roman.njoroge@njuguna.com>
2023-02-07 22:26:35 +0300

    feat: Adding command to insert user in database

--- src/Users/db_users.js
+++ src/Users/db_users.js
@@ -1,5 +1,7 @@
 const checkIfUserInDB = "SELECT fname FROM User2 WHERE username = $1";
+const addUser = "INSERT INTO User2 VALUES ($1, $2, $3, $4, $5, $6)";
 
 module.exports = {
     checkIfUserInDB,
+    addUser
 }
