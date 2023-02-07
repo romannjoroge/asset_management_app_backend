c8e9118fcde7c131040a4fe664b74302609cbbdc
Roman Njoroge <roman.njoroge@njuguna.com>
2023-02-07 22:05:56 +0300

    feat: Adding function to check if location ID exists

--- src/Tracking/location.js
+++ src/Tracking/location.js
@@ -3,5 +3,23 @@ const pool = require('../../db2');
 
 // Importing custom classes
 const MyError = require('../../utility/myError');
-const Folder = require('./folder');
+const Folder = require('../Allocation/folder');
 const utility = require('../../utility/utility');
+const locationTable = require('../Tracking/db_location');
+
+class Location {
+    constructor (){}
+
+    static async verifyLocationID(id, message){
+        // Throws an error if location doesn't exist and returns it's name if it does exist
+        let fetchResult;
+        try{
+            fetchResult = await pool.query(locationTable.getLocation, [id]);
+        }catch(err){
+            throw new MyError(message);
+        }
+        return fetchResult.rows[0].name;
+    }
+}
+
+module.exports = Location;
