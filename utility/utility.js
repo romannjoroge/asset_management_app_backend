e5e5e930b58ec06df76522eebef31b00ee644071
Roman Njoroge <roman.njoroge@njuguna.com>
2023-02-07 17:38:19 +0300

    feat: Adding a function to check if an item is in a list

--- utility/utility.js
+++ utility/utility.js
@@ -15,13 +15,55 @@ function isAnyEmpty(arr){
    return isEmpty
 }
 
-function checkIfBoolean(x, message) {
+function checkIfBoolean(x, errorMessage) {
     if (typeof x !== "boolean"){
-        throw new MyError(message);
+        throw new MyError(errorMessage);
+    }
+}
+
+function checkIfNumberisPositive(x, errorMessage){
+    if (!Number.isInteger(x) || x < 0){
+        throw new MyError(errorMessage);
+    }
+}
+
+function checkIfValidDate(x, errorMessage){
+    let splitDate = x.split('-');
+    let month = splitDate[0];
+    let day = splitDate[1];
+    let year = splitDate[2];
+    let dateFromX = new Date(year, month - 1, day);
+
+    // Throws an error if x is not a date or cannot be parsed to a date otherwise returns a date object
+    if (Object.prototype.toString.call(dateFromX) === "[object Date]"){
+        // x is an instance of date
+        if(isNaN(dateFromX)){
+            throw new MyError(errorMessage);
+        }else{
+            return dateFromX
+        }
+    }else{
+        throw new MyError(errorMessage);
+    }
+}
+
+function verifyDatabaseFetchResults(fetchResult, errorMessage){
+    if(fetchResult.rowCount === 0){
+        throw new MyError(errorMessage);
+    }
+}
+
+function checkIfInList(list, item, errorMessage){
+    // This function throws an error containing errorMessage if item is not in list
+    if (!list.includes(item)){
+        throw new MyError(errorMessage);
     }
 }
 
 module.exports = {
     isAnyEmpty,
-    checkIfBoolean
+    checkIfBoolean,
+    checkIfNumberisPositive,
+    checkIfValidDate,
+    verifyDatabaseFetchResults
 }
