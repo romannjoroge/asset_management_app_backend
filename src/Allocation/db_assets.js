32bc4794cc298783e87c7fa404c83f6107738f9a
Roman Njoroge <roman.njoroge@njuguna.com>
2023-02-07 22:31:08 +0300

    feat: Adding command to add file attachment and asset to asset register

--- src/Allocation/db_assets.js
+++ src/Allocation/db_assets.js
@@ -1,5 +1,11 @@
 const doesAssetTagExist = "SELECT isFixed FROM Asset WHERE assetTag = $1";
+const addAssetToAssetRegister =`INSERT INTO Asset (assetTag, makeAndModelNo, isFixed, serialNumber, acquisitionDate, locationID,
+    status, custodianName, acquisitionCost, insuranceValue, categoryID, assetLifeSpan) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`;
+const addAssetFileAttachment = "INSERT INTO Asset_File (assetTag, attachment) VALUES ($1, $2)";
+
 
 module.exports = {
-    doesAssetTagExist
+    doesAssetTagExist,
+    addAssetToAssetRegister,
+    addAssetFileAttachment
 }
