043641dac809a9f5d21db56679006a0e136bd12b
romannjoroge <roman.njoroge@njuguna.com>
2023-02-13 11:34:14 +0300

    feat: Adding connection to test database

--- db2.js
+++ db2.js
@@ -3,11 +3,21 @@ const dotenv = require('dotenv')
 dotenv.config()
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user: process.env.DATABASEUSER2,
-    database: process.env.DATABASE2,
+    user: process.env.TESTDATABASEUSER,
+    database: process.env.TESTDATABASE,
     port: process.env.PORT,
     password: process.env.PASSWORD
 })
 
+async function testDatabaseConnection(){
+    try{
+        const fetchResult = await pool.query('SELECT NOW()')
+        console.log(fetchResult);
+    }catch(err){
+        console.log(err);
+    }
+}
+
+testDatabaseConnection();
 
 module.exports = pool
