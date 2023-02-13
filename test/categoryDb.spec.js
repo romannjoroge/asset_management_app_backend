198c3dc670d432d02c18d307a109e782fcc865f7
romannjoroge <roman.njoroge@njuguna.com>
2023-02-14 00:35:49 +0300

    test: Writting tests for getCategoryID

--- test/categoryDb.spec.js
+++ test/categoryDb.spec.js
@@ -1,1 +1,47 @@
-// Tests for the functions interacting with the database
+// Importing pool
+const pool = require('../db2');
+
+const assert = require('chai').assert;
+
+const Category = require('../src/Allocation/category2');
+const utility = require('../utility/utility');
+
+describe("getCategoryID Tests", function(){
+    let categoryName;
+    let categoryID;
+    let errorMessage;
+
+    beforeEach(async function(){
+        categoryName = "Existing";
+        categoryID = 3;
+        errorMessage = "Category Does Not Exist";
+
+        try{
+            await pool.query("CREATE TEMPORARY TABLE Category (LIKE Category INCLUDING ALL)");
+            await pool.query("INSERT INTO Category (ID, name, parentFolderID, depreciationType) VALUES ($1, $2, 1, 'Double Declining Balance')", [categoryID, categoryName]);
+        }catch(err){
+            console.log(err);
+            assert(false, "Could Not create temporary databases");
+        }
+    });
+
+    it("should throw an error when category name does not exist", async function(){
+        // Test Inputs
+        categoryName = "Does Not Exist";
+
+        await utility.assertThatAsynchronousFunctionFails(Category.getCategoryID, errorMessage, categoryName, errorMessage);
+    });
+
+    it("should return a category ID when a valid category ID is given", async function(){
+        await utility.assertThatDBFunctionReturnsRightThing(Category.getCategoryID, categoryID, "id", categoryName, errorMessage);
+    });
+
+    afterEach(async function(){
+        try{
+            await pool.query("DROP TABLE IF EXISTS pg_temp.Category");
+        }catch(err){
+            console.log(err);
+            assert(false, "Could Not Delete Temporary Table");
+        }
+    })
+});
