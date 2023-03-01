1ec9abb6b52cea80202f8d8a6f96b1ebf7e67301
romannjoroge <roman.njoroge@njuguna.com>
2023-03-01 18:21:32 +0300

    feat: Creating Apollo server

--- graphql_server.js
+++ graphql_server.js
@@ -2,3 +2,22 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
 import typeDefs from './graphql_schema';
+import Category from './src/resolvers/category';
+import {Category as Category_class} from './src/Allocation/Category/category2';
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers: {
+        Category,
+    },
+    context: async(req, res) => ({
+        category: Category_class,
+    }),
+});
+
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 5000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
+
