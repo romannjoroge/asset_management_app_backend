e5aba308ac977bd17bbc8173f977ce26f901a92b
Roman Njoroge <roman.njoroge@njuguna.com>
2023-03-06 16:27:36 +0300

    fix: Fixing updates for files

---
+++ routes/category.js
@@ -0,0 +1,6 @@
+import express from 'express';
+const router = express.Router();
+import Asset from '../src/Allocation/Asset/asset2.js';
+import { Errors, Succes } from '../utility/constants.js';
+
+export default router;
--- routes/tracking.js
+++ routes/tracking.js
@@ -1,29 +1,29 @@
-const express = require('express')
-const router = express.Router()
-const{
-    moveItem,
-    displayItem,
-    displayItems,
-    getLocationFromBranch,
-    getBranchNames,
-    addLocation,
-    addBranch
-} = require('../logic/tracking')
+import express from 'express';
+const router = express.Router();
+// const{
+//     moveItem,
+//     displayItem,
+//     displayItems,
+//     getLocationFromBranch,
+//     getBranchNames,
+//     addLocation,
+//     addBranch
+// } = require('../logic/tracking')
 
-const {test} = require('../test/routes_test') 
-// Test to see if the route is reachable
-router.get('/', test)
+// const {test} = require('../test/routes_test') 
+// // Test to see if the route is reachable
+// router.get('/', test)
 
-router.route('/move').get().put(moveItem)
-router.route('/display').get(displayItems)
-router.get('/display/:id', displayItem)
-router.get('/locationFromBranch/:id', getLocationFromBranch)
-router.get('/getBranchNames', getBranchNames)
-router.post('/add/location', addLocation)
-router.post('/add/branch', addBranch)
+// router.route('/move').get().put(moveItem)
+// router.route('/display').get(displayItems)
+// router.get('/display/:id', displayItem)
+// router.get('/locationFromBranch/:id', getLocationFromBranch)
+// router.get('/getBranchNames', getBranchNames)
+// router.post('/add/location', addLocation)
+// router.post('/add/branch', addBranch)
 
 router.route('*', (req, res)=>{
     res.status(404).json({data:'Resource not found'})
 })
 
-module.exports = router
+export default router;
