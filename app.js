fdae62203d00af7fd5d261b842ea5b954f902781
romannjoroge <roman.njoroge@njuguna.com>
2023-03-21 23:06:04 +0300

    feat: Implementing simple login

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ import multer from 'multer'; // Deals with file upload
 const upload = multer({dest: 'attachments/'});  // Specifies path to store uploaded attachments
 import dotenv from 'dotenv';
 dotenv.config()  // Brings varaibles from .env file
+import session from 'express-session'; // Middleware to deal with sessions
+import pool from './db2.js';
+import { Errors } from './utility/constants.js';
 
 // // Auth0 stuff
 // const { auth } = require('express-openid-connect');  // Allows us to communicate with openid compliant services
@@ -23,8 +26,16 @@ dotenv.config()  // Brings varaibles from .env file
 
 // Reading JSON data from forms and JS respectively
 app.use(cors());
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        sameSite: 'strict',
+    }
+}));
 
 // Importing routes
 import allocate from './routes/allocation.js';
@@ -42,15 +53,48 @@ app.use('/assets/category', category)
 app.use('/tracking', tracking)
 app.use('/gatepass', gatepass)
 app.use('/reports', reports)
-app.use('/users', users)
+app.use('/users', users);
 
 app.get('/', (req, res)=>{
-    console.log(req.oidc.isAuthenticated())
+    console.log(req.session);
     res.status(200).json({success: true, msg: 'Secured resource'})
-})
+});
 
 app.get('/assets', (req, res)=>{
     res.status(200).json({'success':true, 'data':'To Be Determined'})
+});
+
+app.post('/login', (req, res) => {
+    // Check if there is already a cookie
+    if (req.session.user) {
+        // User already has a cookie
+        return res.status(200).json({
+            message: "Already Logged In"
+        })
+    } else {
+        // Check if user exists
+        const {username, password} = req.body;
+        pool.query("SELECT username FROM User2 WHERE username = $1 AND password = $2", [username, password]).then(data => {
+            // If no data is returned then user does not exist
+            if (data.rowCount == 0) {
+                return res.status(404).json({
+                    message: "User Not Found"
+                })
+            }
+            // User does exist
+            req.session.user = {
+                name: data.rows[0],
+            }
+            return res.status(200).json({
+                message: "Logged In!"
+            })
+        }).catch(err => {
+            console.log(err);
+            return res.status(501).json({
+                message: Errors[9]
+            });
+        })
+    }
 })
 
 app.route('*', (req, res)=>{
