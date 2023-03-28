79ce1e3178af100397e8f72a563e33c751eb0edc
romannjoroge <roman.njoroge@njuguna.com>
2023-03-28 21:41:27 +0300

    feat: Adding routes for view user page

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ import pool from '../db2.js';
 
 router.get('/getUsers', (req, res) => {
     pool.query(userTable.getUsers, []).then(data => {
-        if(data.rowCount <= 0) {
+        if (data.rowCount <= 0) {
             return res.status(404).json({
                 message: Errors[14],
             });
@@ -20,5 +20,57 @@ router.get('/getUsers', (req, res) => {
         })
     })
 });
+
+router.get('/userTable', (req, res) => {
+    // Get usernames and emails from database
+    pool.query(userTable.nameEmail, []).then(data => {
+        // If data is empty return an error
+        if (data.rowCount <= 0) {
+            return res.status(400).json({ message: Errors[22] });
+        }
+
+        // Return data
+        return res.json(data.rows);
+    }).catch(err => {
+        console.log(err);
+        return res.status(500).json({ message: Errors[9] });
+    })
+});
+
+router.get('/user/:id', (req, res) => {
+    // Get username
+    const username = req.params.id;
+
+    // Get email of user
+    pool.query("SELECT email FROM User2 WHERE username=$1", [username]).then(data => {
+        // Return an error if data is empty
+        if (data.rowCount == 0) {
+            return res.status(400).json({ message: Errors[22] });
+        }
+
+        let email = data.rows[0]['email']
+        // console.log(email);
+        // return res.send("OK");
+        // Get roles
+        pool.query(userTable.userRoles, [username]).then(data => {
+            // Return an error if data is empty
+            if (data.rowCount == 0) {
+                return res.status(400).json({ message: Errors[22] });
+            }
+            // Extract roles
+            let roles = data.rows.map(row => {
+                return row['name'];
+            })
+            // Send roles and email
+            return res.json({email, roles})
+        }).catch(e => {
+            console.log(e);
+            return res.status(500).json({ message: Errors[9] });
+        })
+    }).catch(e => {
+        console.log(e);
+        return res.status(500).json({ message: Errors[9] });
+    })
+})
 
 export default router;
--- src/Users/db_users.js
+++ src/Users/db_users.js
@@ -1,13 +1,17 @@
 const checkIfUserInDB = "SELECT fname FROM User2 WHERE username = $1";
 const addUser = "INSERT INTO User2 VALUES ($1, $2, $3, $4, $5, $6)";
 const getUsers = "SELECT username FROM User2";
-const isUserAuthorized = "SELECT username FROM UserRole WHERE roleID=(SELECT id FROM Role WHERE name=$1) AND username=$2"
+const isUserAuthorized = "SELECT username FROM UserRole WHERE roleID=(SELECT id FROM Role WHERE name=$1) AND username=$2";
+const nameEmail = "SELECT username, email FROM User2";
+const userRoles = "SELECT name FROM Role WHERE id IN (SELECT roleid FROM UserRole WHERE username=$1)"
 
 let userTable = {
     checkIfUserInDB,
     addUser,
     getUsers,
-    isUserAuthorized
+    isUserAuthorized,
+    nameEmail,
+    userRoles
 }
 
 export default userTable;
