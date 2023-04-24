b8a66ef93d9cf202c023f3ad0ee668b97b3ecd38
romannjoroge <roman.njoroge@njuguna.com>
2023-04-24 22:57:54 +0300

    fix: Returning description instead of asset id

--- src/Allocation/Asset/db_assets.js
+++ src/Allocation/Asset/db_assets.js
@@ -26,7 +26,7 @@ const getAssetDetails = `
 `
 const insertAssetTag = `INSERT INTO Tags(commandCode, hardwareKey, tagRecNums, antNo, pc, epcID, crc) VALUES ($1, $2, $3, $4, $5, $6, $7)`;
 const unallocate = `UPDATE Asset SET custodianname = null WHERE assettag = $1`;
-const getAllAssets= "SELECT a.barcode, a.assetID, a.condition, c.name AS category, a.serialNumber, l.name AS location FROM Asset a FULL JOIN Location l ON l.id = a.locationid FULL JOIN Category c ON a.categoryid = c.id";
+const getAllAssets= "SELECT a.barcode, a.description, a.condition, c.name AS category, a.serialNumber, l.name AS location FROM Asset a FULL JOIN Location l ON l.id = a.locationid FULL JOIN Category c ON a.categoryid = c.id";
 const assetCategories = 'SELECT c.name, COUNT(*) FROM Asset a JOIN Category c ON c.id = a.categoryid GROUP BY c.id';
 const deleteAsset = "DELETE FROM Assets WHERE barcode = $1";
 const getAssetNetAndTotal = "SELECT SUM(acquisitionCost) AS netValue, COUNT(*) AS total FROM Asset";
