05e544f2a8bf70d15947bba204219307789e5d60
romannjoroge <roman.njoroge@njuguna.com>
2023-04-25 01:57:04 +0300

    fix: Removing reference to site coz it not exist

--- src/Tracking/db_location.js
+++ src/Tracking/db_location.js
@@ -5,7 +5,7 @@ const createLocation = "INSERT INTO Location (name, companyname, site) VALUES ($
 const doesSiteExist = "SELECT * FROM Site WHERE name = $1 AND companyName = $2"
 const createSite = "INSERT INTO Site (name, county, city, address, companyname) VALUES ($1, $2, $3, $4, $5)"
 const getLocationSites = "SELECT l1.name AS location, l2.name AS parent FROM Location l1 LEFT JOIN Location l2 ON l1.parentlocationid = l2.id"
-const getSites = "SELECT name FROM Site";
+const getSites = "SELECT name FROM Location WHERE parentLocationID IS NULL";
 
 let locationTable = {
     getLocation,
