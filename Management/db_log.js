a92bc0176dbfcedcc96a5c6438aada894e438130
romannjoroge <roman.njoroge@njuguna.com>
2023-06-06 11:04:06 +0300

    refactor: Deleting old unused folders

--- Management/db_log.js
+++
@@ -1,5 +0,0 @@
-const addLog = 'INSERT INTO log(user_id, time_occur, event, action) VALUES($1, $2, $3, $4)'
-
-module.exports = {
-    addLog
-}
--- Management/db_users.js
+++
@@ -1,9 +0,0 @@
-const addUser = 'INSERT INTO Users(user_id, email, username) VALUES($1, $2, $3)'
-const getUser = 'SELECT username FROM Users'
-const getUserFromName = 'SELECT * FROM Users WHERE username=$1'
-
-module.exports = {
-    addUser,
-    getUser,
-    getUserFromName
-}
