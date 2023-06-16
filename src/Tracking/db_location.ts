76e7834c5185e6ff71d5191a9340e14637afd59a
romannjoroge <roman.njoroge@njuguna.com>
2023-06-16 23:11:43 +0300

    fix: Only checking locations that arent deleted

--- src/Tracking/db_location.ts
+++ src/Tracking/db_location.ts
@@ -1,6 +1,6 @@
 let getLocation = "SELECT name FROM Location WHERE id = $1";
 let getLocations = "SELECT name, id FROM Location"
-const doesLocationExist = "SELECT * FROM Location WHERE name = $1 AND parentLocationID IN (SELECT id FROM Location WHERE name = $2 AND companyName = $3)"
+const doesLocationExist = "SELECT * FROM Location WHERE name = $1 AND parentLocationID IN (SELECT id FROM Location WHERE name = $2 AND companyName = $3) AND deleted = false"
 const createLocation = "INSERT INTO Location (name, companyname, parentLocationID) VALUES ($1, $2, (SELECT id FROM Location WHERE name = $3 AND companyName=$4))"
 const doesSiteExist = "SELECT * FROM Site WHERE name = $1 AND companyName = $2"
 const createSite = "INSERT INTO Site (name, county, city, address, companyname) VALUES ($1, $2, $3, $4, $5)"
