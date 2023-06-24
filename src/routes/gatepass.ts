1923a8b118767db7cc9055defd017f3ced63b662
romannjoroge <roman.njoroge@njuguna.com>
2023-06-25 01:39:51 +0300

    fix: loggin req

--- src/routes/gatepass.ts
+++ src/routes/gatepass.ts
@@ -144,6 +144,7 @@ router.put('/updateReader', (req, res) => {
 });
 
 router.get('/getPastGatePasses', (req, res) => {
+    console.log('\n\n');
     console.log(req);
     // Send past requests
     getPastRequests(req.username).then(data => {
