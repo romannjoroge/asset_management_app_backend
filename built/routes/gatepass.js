bf783af39e3b202c507e0c02454e2ae62b89d627
romannjoroge <roman.njoroge@njuguna.com>
2023-06-25 01:39:15 +0300

    fix: Console logging req

--- built/routes/gatepass.js
+++ built/routes/gatepass.js
@@ -127,6 +127,7 @@ router.put('/updateReader', (req, res) => {
     });
 });
 router.get('/getPastGatePasses', (req, res) => {
+    console.log(req);
     // Send past requests
     getPastRequests(req.username).then(data => {
         return res.json(data);
