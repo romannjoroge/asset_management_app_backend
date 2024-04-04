115f01813be9a04e714d50d2b5033986469db036
Roman Njoroge <roman.njoroge@njuguna.com>
2024-04-04 23:33:16 +0300

    fix: Getting gatepass requests of user

--- built/GatePass/db_gatepass.js
+++ built/GatePass/db_gatepass.js
@@ -9,7 +9,7 @@ const addGateAuthorizer = 'INSERT INTO AuthorizeGatepass (userid, gatepassid) VA
 const getPreviousGatePasses = `SELECT g.id, u.name, (SELECT name AS 
     fromlocation FROM Location WHERE id = g.fromlocation), (SELECT name AS tolocation FROM Location WHERE id = 
         g.tolocation), g.date, a.barcode, g.reason, g.approved FROM Gatepass g JOIN gatepassasset ga ON ga.gatepassid = g.id JOIN 
-        Asset a ON a.assetid = ga.assetid LEFT JOIN User2 u ON u.id = g.userid WHERE g.userid = &1 AND g.deleted = false;
+        Asset a ON a.assetid = ga.assetid LEFT JOIN User2 u ON u.id = g.userid WHERE g.userid = $1 AND g.deleted = false;
 `;
 const getRequestedGatePasses = `SELECT g.id,(SELECT name FROM User2 WHERE userid = g.userid LIMIT 1), (SELECT name AS fromlocation FROM Location WHERE id = g.fromlocation), (SELECT name AS tolocation FROM Location WHERE id = g.tolocation), g.date, a.barcode, g.reason, g.approved 
                                 FROM Gatepass g LEFT JOIN location l ON l.id = g.fromlocation LEFT JOIN location ON l.id = g.tolocation JOIN gatepassasset ga  
