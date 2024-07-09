39980c0e5171ea65228dd116e5a49b99ece6e891
romannjoroge <roman.njoroge@njuguna.com>
2024-07-09 22:36:56 +0300

    feat: Function for creating excel file for any map complete

---
+++ built/Excel/createExcelFileForGeneralData.js
@@ -0,0 +1,27 @@
+import { MyErrors2 } from "../utility/constants.js";
+import MyError from "../utility/myError.js";
+import * as XLSX from 'xlsx/xlsx.mjs';
+import * as fs from 'fs';
+XLSX.set_fs(fs);
+export function loadDataToExcelFile(data, sheetName) {
+    try {
+        // Create a worksheet with the data
+        const worksheet = XLSX.utils.json_to_sheet(data);
+        // Attach worksheet to workbook
+        const workbook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(workbook, worksheet, "Assets");
+        // Fixing headers
+        XLSX.utils.sheet_add_aoa(worksheet, [Object.keys(data[0])], { origin: "A1" });
+        // Fixing maximum width of column
+        const max_width = data.reduce((w, r) => Math.max(w, r.description.length), 10);
+        worksheet["!cols"] = [{ wch: max_width }];
+        return (XLSX.write(workbook, {
+            type: 'buffer'
+        }));
+    }
+    catch (err) {
+        console.log("Error Loading Data To Excel File", err);
+        throw new MyError(MyErrors2.NOT_CREATE_EXCEL_FILE);
+    }
+}
+//# sourceMappingURL=createExcelFileForGeneralData.js.map
